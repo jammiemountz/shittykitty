df4b4916660b5eadaa5248e542ab89e1cdeee32f
Jammie Mountz <jmmountz@gmail.com>
2016-10-01 21:31:42 -0700

    procfile

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var pg = require('pg');
+
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
