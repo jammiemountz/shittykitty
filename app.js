4ccb02cb8ad68a302a36e26458ee80caf19c020b
Jammie Mountz <jmmountz@gmail.com>
2016-10-02 12:06:07 -0700

    api running

--- app.js
+++ app.js
@@ -2,61 +2,59 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var pg = require('pg');
-
+var query = require('pg-query');
 pg.defaults.ssl = true;
-console.log('----------', process.env.DATABASE_URL)
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-// pg.connect("//yzjbpbvlspnfki:MXpfTCsB-TOY2LQd_MbwpX5kbL@ec2-54-225-81-90.compute-1.amazonaws.com:5432/d5sfp9vtqj8hue", function(err, client) {
-  if (err) {
-    console.log(err)
-    throw err;
-  }
-  console.log('Connected to postgres! Getting schemas...');
 
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
+var env = process.env.PORT ? 'prod' : 'dev';
+var port = process.env.PORT ? process.env.PORT : 8080;
+var databaseId = env==='dev' ? 100 : 1;
+var dburl = process.env.DATABASE_URL;
+query.connectionParameters = dburl;
+
+query('CREATE TABLE IF NOT EXISTS items(id SERIAL PRIMARY KEY, amount INTEGER not null)', function() {
+  query('INSERT INTO items VALUES (1,0)');
+  query('INSERT INTO items VALUES (100,0)');
 });
 
-// postgres://yzjbpbvlspnfki:MXpfTCsB-TOY2LQd_MbwpX5kbL@ec2-54-225-81-90.compute-1.amazonaws.com:5432/d5sfp9vtqj8hue
-// app.get('/db', function (request, response) {
-//   // pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-//   pg.connect("//yzjbpbvlspnfki:MXpfTCsB-TOY2LQd_MbwpX5kbL@ec2-54-225-81-90.compute-1.amazonaws.com:5432/d5sfp9vtqj8hue", function(err, client, done) {
-//     client.query('SELECT * FROM test_table', function(err, result) {
-//       done();
-//       if (err)
-//        { console.error(err); response.send("Error " + err); }
-//       else
-//        { response.render('pages/db', {results: result.rows} ); }
-//     });
-//   });
-// });
-
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
 app.use('/', express.static(__dirname + '/'));
 
-// viewed at http://localhost:8080
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('/api/pooped', function(req, res) {
-    //send to database
-    res.end()
+  var amount;
+  query('SELECT * FROM items WHERE id=' + databaseId,function(err, rows, result){
+    rows.forEach(function(row) {
+      amount = row.amount + 1;
+    })
+    query('UPDATE items SET amount=' + amount + ' WHERE id=' + databaseId, function(err, rows, result) {
+      if (err) {
+        res.send(err);
+        res.end();
+      }
+      res.send(JSON.stringify({ data: 'success' }))
+      res.end();
+    })
+  });
 });
 
 app.get('/api/getpoop', function(req, res) {
-    //retrieve from database
-    res.write('2')
-    res.end()
+  var amount;
+  query('SELECT * FROM items WHERE id=' + databaseId,function(err, rows, result){
+    rows.forEach(function(row) {
+      amount = row.amount;
+    })
+    res.send(JSON.stringify({ data: amount }));
+    res.end();
+  });
 });
 
-console.log('live at http://localhost:8080/')
-app.listen(process.env.PORT || 8080);
+console.log('RUNNING ENVIRONMENT ' + env + ' ON PORT ' + port + ' USING DB ID ' + databaseId);
+app.listen(port);
